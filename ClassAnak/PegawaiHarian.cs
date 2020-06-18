734132f828cb2319c643f08df34df866b12bcaf2
Anjar1912 <61839743+Anjar1912@users.noreply.github.com>
2020-06-18 21:18:31 +0700

    Tugas Lab 8 (Pertemuan 11)

---
+++ ClassAnak/PegawaiHarian.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tugas_Lab_8.ClassInduk;
+
+namespace Tugas_Lab_8.ClassAnak
+{
+    public class PegawaiHarian : Pegawai
+    {
+        public int JumlahJamKeja { get; set; }
+        public int UpahPerJam { get; set; }
+        public override double Gaji()
+        {
+            return JumlahJamKeja * UpahPerJam;
+        }
+
+    }
+}
