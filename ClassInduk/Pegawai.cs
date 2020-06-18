ee5f435c263bcc2b9684289000a55d8112e718d0
Anjar1912 <61839743+Anjar1912@users.noreply.github.com>
2020-06-18 21:32:22 +0700

    Tugas Lab 8 (Pertemuan 11)

--- ClassInduk/Pegawai.cs
+++ ClassInduk/Pegawai.cs
@@ -1,1 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace Tugas_Lab_8.ClassInduk
+{
+   public abstract class Pegawai
+    {
+        public string Nama { get; set; }
+        public string Nik { get; set; }
+        public abstract double Gaji();
+    }
+}
